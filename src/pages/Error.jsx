c4ccaa5e1c14eb0022dba873f839c092bb3354cf
莓莓Joy <pai314125@163.com>
2022-03-16 00:22:03 +0800

    test

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Error.scss";
+
+function Error() {
+  return (
+    <div className="error">
+      <h1>404---------</h1>
+    </div>
+  );
+}
+
+export default Error;
